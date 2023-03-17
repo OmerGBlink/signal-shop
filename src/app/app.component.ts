76efab1dfd5dc23a96c16650e04017631b12269d
omergronich <gronichomer@gmail.com>
2023-03-17 15:27:00 +0200

    race condition example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
-
+import { Component, effect, OnInit, signal } from '@angular/core';
+import { Product } from './product.type';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <app-category-list
+      *effect
+      class="categories"
+      [categories]="categories()"
+      [selectedCategory]="selectedCategory()"
+      (categorySelected)="selectCategory($event)"
+    ></app-category-list>
+
+    <div *effect class="grid">
+      <mat-card *ngFor="let product of products()" class="example-card">
+        <mat-card-header>
+          <mat-card-title>{{ product.title }}</mat-card-title>
+        </mat-card-header>
+        <img mat-card-image [src]="product.image" />
+      </mat-card>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'signal-shop';
+  products = signal<Product[]>([]);
+  categories = signal<string[]>([]);
+
+  selectedCategory = signal<string>('All');
+
+  constructor(private http: HttpClient) {
+    this.getCategories();
+    this.getProducts();
+  }
+
+  getProducts() {
+    firstValueFrom(
+      this.http.get<Product[]>('https://fakestoreapi.com/products')
+    ).then((products) => this.products.set(products));
+  }
+
+  getCategories() {
+    firstValueFrom(
+      this.http.get<string[]>('https://fakestoreapi.com/products/categories')
+    ).then((categories) => this.categories.set(['All', ...categories]));
+  }
+
+  getProductsByCategory(category: string) {
+    firstValueFrom(
+      this.http.get<Product[]>(
+        `https://fakestoreapi.com/products/category/${category}`
+      )
+    ).then((products) => {
+      this.products.set(products);
+    });
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory.set(category);
+
+    if (this.selectedCategory() === 'All') {
+      this.getProducts();
+    } else {
+      this.getProductsByCategory(this.selectedCategory());
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { EffectDirective } from './effect.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { NgOptimizedImage } from '@angular/common';
+import { RaceConditionSimulatorInterceptor } from './race-condition-simulator.interceptor';
+import { CategoryListComponent } from './category-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, EffectDirective, CategoryListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    NgOptimizedImage,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RaceConditionSimulatorInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
