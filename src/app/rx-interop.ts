d126e87c8a5e43a3749547fd858ac85e5433b418
omergronich <gronichomer@gmail.com>
2023-03-18 13:22:37 +0200

    changed fromObservable type

--- src/app/rx-interop.ts
+++ src/app/rx-interop.ts
@@ -1,18 +1,28 @@
-import {DestroyRef, effect, inject, SettableSignal, signal, Signal} from "@angular/core";
-import {Observable, Subject, takeUntil} from "rxjs";
+import {
+  DestroyRef,
+  effect,
+  inject,
+  SettableSignal,
+  signal,
+  Signal,
+} from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
-export const fromObservable = <T>(obs$: Observable<T>, initialValue?: T): Signal<T> => {
+export const fromObservable = <T>(
+  obs$: Observable<T>,
+  initialValue?: T
+): SettableSignal<T> => {
   const s = signal(initialValue as T);
   const destroyRef = inject(DestroyRef);
   const destroy$ = new Subject<void>();
   destroyRef.onDestroy(() => {
     destroy$.next();
-  })
+  });
   obs$.pipe(takeUntil(destroy$)).subscribe((v) => {
     s.set(v);
   });
   return s;
-}
+};
 
 export const fromSignal = <T>(sig: Signal<T>): Observable<T> => {
   const destroyRef = inject(DestroyRef);
@@ -20,29 +30,31 @@ export const fromSignal = <T>(sig: Signal<T>): Observable<T> => {
   return new Observable<T>((observer) => {
     const disposer = effect(() => {
       observer.next(sig());
-    })
+    });
 
-    destroyRef.onDestroy(disposer.destroy)
+    destroyRef.onDestroy(disposer.destroy);
   });
-}
+};
 
-
-export const registerRxEffect = <T>(obs$: Observable<T>, effectFn: (x: T) => void) => {
+export const registerRxEffect = <T>(
+  obs$: Observable<T>,
+  effectFn: (x: T) => void
+) => {
   const destroyRef = inject(DestroyRef);
   const destroy$ = new Subject<void>();
   destroyRef.onDestroy(() => {
     destroy$.next();
-  })
+  });
   obs$.pipe(takeUntil(destroy$)).subscribe(effectFn);
-}
+};
 
 export const connect = <T>(signal: SettableSignal<T>, obs$: Observable<T>) => {
   const destroyRef = inject(DestroyRef);
   const destroy$ = new Subject<void>();
   destroyRef.onDestroy(() => {
     destroy$.next();
-  })
-  obs$.pipe(takeUntil(destroy$)).subscribe({
-    next: signal.set
   });
-}
+  obs$.pipe(takeUntil(destroy$)).subscribe({
+    next: signal.set,
+  });
+};
